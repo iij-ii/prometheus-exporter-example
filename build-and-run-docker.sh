3b5f196ee748ce17d902dc25a54c1f5a3afe4162
Yuji Yamamoto <yuji-yamamoto@iij.ad.jp>
2019-10-29 18:28:43 +0900

    Add command to build static binary of the exporter

--- build-and-run-docker.sh
+++ build-and-run-docker.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+cabal --with-ghc=`stack exec which ghc` build --enable-executable-static
+
 docker build -t iijii/prometheus-exporter .
 
 docker run --name prometheus-exporter -p 9090:9090 -p 8080:8080 iijii/prometheus-exporter
